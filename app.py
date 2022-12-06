01530de47cbdc648628d647310eeb91f08e71988
Akshaysat <akshay.satpaise99@gmail.com>
2022-12-06 17:13:02 +0530

    added la logo

--- app.py
+++ app.py
@@ -18,6 +18,17 @@ hide_streamlit_style = """
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
+col1, col2, col3 = st.columns(3)
+with col1:
+    st.write("")
+with col2:
+    st.image("logo.png", width=225)
+    st.write("")
+with col3:
+    st.write("")
+
+st.write("----")
+
 st.markdown(
     "<h2 style='text-align: center; color: white;'>Module Completion Calculator</h2>",
     unsafe_allow_html=True,
